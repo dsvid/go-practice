4163ceb753b9c4f235dfe1ff98d5dbc03fff54dd
David La Roche <davidlaroche@Davids-MacBook-Air.local>
2025-05-19 20:49:54 +1000

    added default behaviour with no argument

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("David")
-	want := "Hello, David"
+	t.Run("hello using names", func(t *testing.T) {
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		got := Hello("David")
+		want := "Hello, David"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("default to 'Hello, World' when called with an empty string", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
