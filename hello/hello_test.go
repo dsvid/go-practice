ca08fc8c1db28e2d9af57bb0c0278ca97c912598
David La Roche <davidlaroche@Davids-MacBook-Air.local>
2025-05-19 23:15:41 +1000

    added helper function

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -7,17 +7,18 @@ func TestHello(t *testing.T) {
 
 		got := Hello("David")
 		want := "Hello, David"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("default to 'Hello, World' when called with an empty string", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
